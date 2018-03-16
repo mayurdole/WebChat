db3eaf8ba670706e599d721b589c7749e4884d31
Nabeel on the Lenovo <you@example.com>
2018-03-16 21:15:24 +0000

    Created HTML pages using Bootstrap and started adding sockets

--- src/main/java/com/nab/chat/controller/ChatController.java
+++ src/main/java/com/nab/chat/controller/ChatController.java
@@ -1,14 +1,28 @@
 package com.nab.chat.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+import com.nab.chat.domain.ChatMessage;
+
+@Controller
 public class ChatController {
 
-	@RequestMapping("/")    
-    public String home() {
-        return "Hello Worlds!";
+	@MessageMapping("/send")
+	@SendTo("/chatroom")    
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
     }
+	
+	@GetMapping("/")
+	public String home() {
+		return "login"; 
+	}
+	
+	@GetMapping("/chat")
+	public String chat() {
+		return "chat"; 
+	}
 }
---
+++ src/main/java/com/nab/chat/domain/ChatMessage.java
@@ -0,0 +1,23 @@
+package com.nab.chat.domain;
+
+public class ChatMessage {
+	private String name;
+	private String message;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
